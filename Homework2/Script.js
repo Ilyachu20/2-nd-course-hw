7682a6a44423ff8fc1e7a7fbd490973fc9527031
Ilyachu20 <ilya.chulanov2058902323@gmail.com>
2025-11-07 16:18:40 +0300

    готовое

---
+++ Homework2/Script.js
@@ -0,0 +1,57 @@
+// Задание 1.
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2.
+let year = 2007;
+alert(year);
+
+// Задание 3.
+let author = 'Brendan Eich';
+alert(author);
+
+// Задание 4.
+let c = 10;
+let d = 2;
+alert(c + d);
+alert(c - d);
+alert(c * d);
+alert(c / d);
+
+// Задание 5.
+let f = 2;
+result = f ** 5;
+alert(result);
+
+// Задание 6.
+let h = 9;
+let g = 2;
+alert(h % g);
+
+// Задание 7.
+let num = 1;
+num += 5;
+num -=3;
+num *=7;
+num /=3;
+num++;
+num--;
+
+// Задание 8.
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+// Задание 9.
+const user = {
+    name:'Jhony',
+    age: 35,
+    isAdmin: True
+};
+
+// Задание 10.
+let name = prompt("Введите Ваше имя");
+alert(`Привет, ${name}!`);
+
+
