9af709c50bcbff65ddec8ba1346ededfa87873a3
Ilyachu20 <ilya.chulanov2058902323@gmail.com>
2026-01-28 17:10:55 +0300

    Готовое на проверку 28.01

---
+++ Homework10/third_game.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // кнопка для десктопа
+  const desktopBtn = document.getElementById("desktop-guess-btn3");
+  if (desktopBtn) {
+    desktopBtn.addEventListener("click", startThird);
+  }
+
+  // копка для мобилки и планшета
+  const mobileBtn = document.getElementById("guess-number-btn3");
+  if (mobileBtn) {
+    mobileBtn.addEventListener("click", startThird);
+  }
+});
+
+// начало игры
+function startThird() {
+  let text = prompt("Введите текст, который будет перевернут");
+  let reversed = text.split("").reverse().join("");
+  alert(reversed);
+}
